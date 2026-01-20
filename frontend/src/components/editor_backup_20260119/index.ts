70d1924a7e775dd1cdd006abb3b37da2512f04c9
G00120 <215266197@qq.com>
2026-01-20 23:30:24 +0800

    chore: 删除无用代码

--- frontend/src/components/editor_backup_20260119/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 编辑器组件统一导出
- */
-export { Player } from './Player';
-export { Timeline } from './Timeline';
-export { Sidebar } from './Sidebar';
-export { PropertiesPanel } from './PropertiesPanel/index';
-export { VideoEditor } from './VideoEditor';
